0119f8fdafcec0e1518216dae815983fd142a269
richietam <richardtam.ny@gmail.com>
2019-04-03 10:10:00 -0400

    updates

--- challenge.js
+++ challenge.js
@@ -1,3 +1,4 @@
+
 // some code to change innerHTML = counter for each interval
 
 
@@ -30,45 +31,82 @@ buttonPlus.addEventListener('click', function (event) {
 const buttonLOVE = document.getElementById("<3")
 
 
+//
+// function countInArray(arrayofCountersLikes) {
+//
+//     let n = 0;
+//     for (let i = 0; i < arrayofCountersLikes.length; i++) {
+//         if (array[i] === counter) {
+//             n++;
+//         }
+//     }
+//     return n;
+// }
 
-function countInArray(arrayofCountersLikes) {
+// const ul = document.querySelector("ul")
 
-    let n = 0;
-    for (let i = 0; i < arrayofCountersLikes.length; i++) {
-        if (array[i] === counter) {
-            n++;
-        }
-    }
-    return n;
-}
+// const newarr = document.querySelector("ul").getElementsByTagName('li')
+// const arrayoflis = Array.from(newarr)
+// const arrayofinnertext = arrrayoflis.map(e => e.innerText.split(" ")[0] )
 
 
-const createli = function () {
+// const createli = function () {
+//   let ul = document.querySelector("ul");
+//   let li = document.createElement("li");
+//
+//
+//    li.appendChild(document.createTextNode(`${counter} has been liked ${likeTracker[counter]} times`));
+//    ul.appendChild(li);
+// }
 
+const addorupdateli = function () {
   let ul = document.querySelector("ul");
   let li = document.createElement("li");
-   li.appendChild(document.createTextNode(`${counter} has been liked ${likeTracker[counter]} times`));
-   ul.appendChild(li);
+
+  if (likeTracker[counter] == 1) {
+    li.appendChild(document.createTextNode(`${counter} has been liked ${likeTracker[counter]} times`));
+    ul.appendChild(li);
+  } else {
+    newarr = document.querySelector("ul").getElementsByTagName('li')
+    arrayoflis = Array.from(newarr)
+    arrayofinnertext = arrayoflis.map(e => e.innerText.split(" ")[0] )
+   index = arrayofinnertext.indexOf(`${counter}`)
+   newarr[index].innerText = `${counter} has been liked ${likeTracker[counter]} times`
+  }
 }
 
-buttonLOVE.addEventListener('click', function (event) {
-  likeTracker
-    updatelikeTracker()
+// const updateli = function () {
+//    newarr = document.querySelector("ul").getElementsByTagName('li')
+//    arrayoflis = Array.from(newarr)
+//    arrayofinnertext = arrayoflis.map(e => e.innerText.split(" ")[0] )
+//   index = arrayofinnertext.indexOf(`${counter}`)
+//   newarr[index].innerText = `${counter} has been liked ${likeTracker[counter]} times`
+// }
 
-      createli()
-      // iterate over ul and count unique numbers
-    })
+// buttonLOVE.addEventListener('click', function () {
+//   updatelikeTracker();
+//
+//   if (Object.keys(likeTracker).length == 1) {
+//     createli()
+//
+//   } else if (!Object.keys(likeTracker).includes(`${counter}`)) {
+//     createli()
+//   debugger
+//   } else {
+//     updateli()
+//   }
+//   }
+// )
+
+
+buttonLOVE.addEventListener('click', function (event) {
+  likeTracker;
+  updatelikeTracker();
+    addorupdateli()
+})
 
 
 
 const updatelikeTracker = function() {
-  arr = Object.keys(likeTracker)
-
-  if (Object.keys(likeTracker).length === 0){
-    likeTracker[counter] = 1
-  }else if (arr.includes(counter)) {
-    (likeTracker[counter] + 1) 
-  }else {
-    likeTracker[counter] = 1
-  }
+  likeTracker[counter] = (likeTracker[counter]) + 1 || 1
 }
